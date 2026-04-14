7a396efb8e01f2d5ce8cd5d7dd31d8af5d6a97a9
Julian Kromer <julian@kromer.in>
2026-04-14 15:09:25 +0200

    Implement basic lib.rs setup

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -1,14 +1,36 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+mod db;
+mod error;
+mod task;
+
+pub use error::{CoreError, CoreResult};
+pub use task::Task;
+
+use db::Database;
+use std::sync::Mutex;
+
+pub struct AppState {
+    db: Mutex<Database>,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+impl AppState {
+    pub fn new(db_path: &str) -> CoreResult<Self> {
+        let db = Database::open(db_path)?;
+        Ok(Self { db: Mutex::new(db) })
+    }
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    pub fn add_task(&self, title: &str) -> CoreResult<Task> {
+        self.db.lock().unwrap().add_task(title)
+    }
+
+    pub fn list_tasks(&self) -> CoreResult<Vec<Task>> {
+        self.db.lock().unwrap().list_tasks()
+    }
+
+    pub fn toggle_tasks(&self, id: i64) -> CoreResult<bool> {
+        self.db.lock().unwrap().toggle_task(id)
+    }
+
+    pub fn delete_task(&self, id: i64) -> CoreResult<bool> {
+        self.db.lock().unwrap().delete_task(id)
     }
 }
