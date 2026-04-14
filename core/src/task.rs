7dfa0257f4257511d1f202732fae5106771ea730
Julian Kromer <julian@kromer.in>
2026-04-14 15:01:00 +0200

    Fix type title now String

--- core/src/task.rs
+++ core/src/task.rs
@@ -3,7 +3,7 @@ use serde::{Deserialize, Serialize};
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Task {
     pub id: i64,
-    pub title: bool,
+    pub title: String,
     pub is_done: bool,
     pub created_at: String,
 }
